350a8ca21d2e03ae7c8cea7370fd25fc34d72cab
Nolan Willis <nolanwillis38@gmail.com>
2022-04-02 22:50:15 -0400

    Started HW4

---
+++ magic.cpp
@@ -0,0 +1,409 @@
+///***********************************************************
+//*
+//* Name: Nolan Willis
+//*
+//* Email: nwillis@syr.edu    
+//*
+//* Assignment: Assignment 4
+//* 
+//* Environment: Visual Studio Community 2022
+//* 
+//* Date submitted: 
+//* 
+//* References: None
+//* 
+//* Interactions: 
+//   
+//      Press esc to exit 
+//*
+//************************************************************/
+
+#include <iostream>
+
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+
+#define PI 3.14159
+
+using namespace std;
+
+//Global Variables------------------------------------------------------------------------
+
+//Window size
+int height, width;
+
+//Mouse click coordinates
+int mouseX, mouseY;
+
+//Camera position values
+static float eyeX = 6, eyeY = 5, eyeZ = 2;
+static float eyeAngle = 0;  //angle facing
+static float stepSize = 5.0;  //step size
+static float turnSize = 10.0; //degrees to turn
+
+//Value to keep track of selecting state
+bool isSelecting = false;
+
+//Value to keep track of the last selected item
+int itemID = 0;
+
+//Mountain size and positioning
+int mountainZ = -17;
+int mountainScaleX = 4, mountainScaleY = 3;
+
+//--------------------------------------------------------------------------------------
+
+//Function that sets the view mode (ortho or frust)
+void setViewMode()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(90, 1, 1, 150);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+
+//Draws the ground 
+void drawBridge()
+{
+    //Floor
+    glColor3f(101.0 / 255.0, 102.0 / 255.0, 105.0 / 255.0);
+    glPushMatrix();
+    glTranslated(6, 0, -6.5);
+    glScaled(5, 1, 12);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Right railing
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(8.5, 1, -6);
+    glScaled(.5, 2, 12);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left railing
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(3.5, 1, -6);
+    glScaled(.5, 2, 12);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+
+void drawCourtyard() 
+{
+    //Floor
+    glColor3f(101.0 / 255.0, 102.0 / 255.0, 105.0 / 255.0);
+    glPushMatrix();
+    glTranslated(6, 0, -22);
+    glScaled(50, 1, 20);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Right front railing
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(19.5, 5, -12);
+    glRotated(90, 0, 1, 0);
+    glScaled(.5, 10, 22.5);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left front railing
+    glPushMatrix();
+    glTranslated(-7.5, 5, -12);
+    glRotated(90, 0, 1, 0);
+    glScaled(.5, 10, 22.5);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Right middle railing
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(31, 5, -23);
+    glScaled(.5, 10, 22.5);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left middle railing
+    glPushMatrix();
+    glTranslated(-19, 5, -23);
+    glScaled(.5, 10, 22.5);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Right back railing
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(23.5, 5, -32);
+    glRotated(90, 0, 1, 0);
+    glScaled(.5, 10, 15.5);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left back railing
+    glPushMatrix();
+    glTranslated(-11.5, 5, -32);
+    glRotated(90, 0, 1, 0);
+    glScaled(.5, 10, 15.5);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+
+void drawHall()
+{
+    //Right wall
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(22.5, 20, -62);
+    glScaled(3, 40, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left wall
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(-11.5, 20, -62);
+    glScaled(3, 40, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Right roof
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(15, 61, -62);
+    glRotated(20, 0, 0, 1);
+    glScaled(3, 50, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Left roof
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(-4, 61, -62);
+    glRotated(-20, 0, 0, 1);
+    glScaled(3, 50, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+    //Center beam for roof
+    glColor3f(114.0 / 255.0, 107.0 / 255.0, 82.0 / 255.0);
+    glPushMatrix();
+    glTranslated(5.5, 85, -62);
+    glRotated(45, 0, 0, 1);
+    glScaled(3.2, 3.2, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    //Floor
+    glColor3f(101.0 / 255.0, 102.0 / 255.0, 105.0 / 255.0);
+    glPushMatrix();
+    glTranslated(6, 0, -62);
+    glScaled(30, 1, 60);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+
+
+//Draws the mountain range
+void drawMountains() 
+{
+    glColor3f(.2, .2, .2);
+    glPushMatrix();
+    glTranslated(-10.0, 0, 0);
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex3f(-1.0 * mountainScaleX, 1.0 * mountainScaleY, mountainZ);
+    glVertex3f(-1.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(0.0 * mountainScaleX, 2.0 * mountainScaleY, mountainZ);
+    glVertex3f(0.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(1.0 * mountainScaleX, 3.0 * mountainScaleY, mountainZ);
+    glVertex3f(1.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(2.0 * mountainScaleX, 3.0 * mountainScaleY, mountainZ);
+    glVertex3f(2.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(3.0 * mountainScaleX, 4.0 * mountainScaleY, mountainZ);
+    glVertex3f(3.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(4.0 * mountainScaleX, 3.0 * mountainScaleY, mountainZ);
+    glVertex3f(4.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(5.0 * mountainScaleX, 2.0 * mountainScaleY, mountainZ);
+    glVertex3f(5.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(6.0 * mountainScaleX, 4.0 * mountainScaleY, mountainZ);
+    glVertex3f(6.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(7.0 * mountainScaleX, 2.0 * mountainScaleY, mountainZ);
+    glVertex3f(7.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(8.0 * mountainScaleX, 5.0 * mountainScaleY, mountainZ);
+    glVertex3f(8.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glVertex3f(9.0 * mountainScaleX, 3.0 * mountainScaleY, mountainZ);
+    glVertex3f(9.0 * mountainScaleX, 0.0 * mountainScaleY, mountainZ);
+    glEnd();
+    glPopMatrix();
+}
+
+//Sets the camera viewing position
+void setCameraView() 
+{
+   gluLookAt(eyeX, eyeY, eyeZ,
+             eyeX + sin(eyeAngle * PI/180), eyeY, eyeZ + cos(eyeAngle * PI/180),
+             0, 1, 0);
+    
+   
+}
+
+//Determines the ID of the object the mouse has clicked 
+void getID(int x, int y) {
+    unsigned char pixel[3];
+    glReadPixels(x, y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, pixel);
+    if ((int)pixel[0] == 0 && (int)pixel[1] == 255 && (int)pixel[2] == 0)
+    {
+        cout << "Helicopter selected" << endl;
+    }
+    else
+    {
+        itemID = 0;
+    }
+    isSelecting = false;
+    glutPostRedisplay();
+}
+
+//Draws the objects in the scene
+void drawObjects(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    //Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+    //Set camera view
+    setCameraView();
+    
+    if (isSelecting)
+    {
+       
+    }
+    else
+    {
+       
+    }
+
+    drawBridge();
+    drawCourtyard();
+    drawHall();
+}
+
+//Drawing routine 
+void drawScene(void)
+{
+    //Draw everything with unique colors but don't display if user isSelecting
+    if (isSelecting)
+    {
+        drawObjects();
+        getID(mouseX, mouseY);
+    }
+    //Draw everything normally
+    else
+    {
+        drawObjects();
+        glutSwapBuffers();
+    }
+}
+
+// Initialization routine
+void setup(void)
+{
+    glClearColor(181.0/255.0, 185.0/255.0, 188.0/255.0, -21.0);
+}
+
+// OpenGL window reshape routine.
+void resize(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    setViewMode();
+    glMatrixMode(GL_MODELVIEW);
+    height = h;
+    width = w;
+}
+
+//Keyboard input processing
+void keyInput(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+        eyeX += stepSize * sin(eyeAngle * PI / 180);
+        eyeZ += stepSize * cos(eyeAngle * PI / 180);
+        glutPostRedisplay();
+        break;
+    case 's':
+        eyeX -= stepSize * sin(eyeAngle * PI / 180);
+        eyeZ -= stepSize * cos(eyeAngle * PI / 180);
+        glutPostRedisplay();
+        break;
+    case 'd':
+        eyeAngle -= turnSize;
+        glutPostRedisplay();
+        break;
+    case 'a':
+        eyeAngle += turnSize;
+        glutPostRedisplay();
+        break;
+    case 27:
+        exit(0);
+        break;
+
+    default:
+        break;
+    }
+}
+
+//Speical keyboard input processing
+void specialkeyInput(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_DOWN:
+        
+        break;
+    case GLUT_KEY_RIGHT:
+        
+        
+        break;
+    case GLUT_KEY_LEFT:
+       
+        break;
+    }
+}
+
+//Mouse input processing
+void mouseInput(int button, int state, int x, int y) 
+{
+    if (button == GLUT_LEFT && state == GLUT_DOWN)
+    {
+        eyeY += 2;
+        glutPostRedisplay();
+    }
+
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+    {
+        eyeY -= 2;
+        glutPostRedisplay();
+    }
+}
+
+//Routine to output interaction instructions to the C++ window
+void printInteraction(void)
+{
+    cout << "   Interaction" << endl;
+    cout << "   Press esc to exit" << endl;
+}
+
+//Main routine
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(1500, 1000);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Magic Castle");
+    setup();
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(resize);
+    glutKeyboardFunc(keyInput);
+    glutSpecialFunc(specialkeyInput);
+    glutMouseFunc(mouseInput);
+    printInteraction();
+    glutMainLoop();
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,170 +1,170 @@
+////
+////  walkingAroundForClass.cpp
+////  Marjory Baruch
+////  builds a ring of numbers, corresponding
+////  to angles in degrees, in a circle of radius
+////  50, centered at the origin.
 //
-//  walkingAroundForClass.cpp
-//  Marjory Baruch
-//  builds a ring of numbers, corresponding
-//  to angles in degrees, in a circle of radius
-//  50, centered at the origin.
-
-//  This version has no lookat.
-
-//  floor is xz plane, positive y is up.
-
-//This define is so you can use sprintf
-#define _CRT_SECURE_NO_DEPRECATE
-
-#include <iostream>
-#include <stdio.h>
-#include <cmath>
-
-#ifdef __APPLE__
-#  include <GLUT/glut.h>
-#else
-#  include <GL/glut.h>
-#endif
-
-#define PI 3.14159
-using namespace std;
-
-//I don't use these by you can
-static float meX = 0, meY = 0, meZ = 0;
-static float angle = 0;  //angle facing
-static float stepsize = 5.0;  //step size
-static float turnsize = 10.0; //degrees to turn
-
-// Initialization routine.
-void setup(void)
-{
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glEnable(GL_DEPTH_TEST); // Enable depth testing.
-}
-
-// OpenGL window reshape routine.
-void resize(int w, int h)
-{
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(120, 1, 1, 100);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-// Routine to draw a stroke character string.
-void writeStrokeString(void* font, const char* string)
-{
-    const char* c;
-    for (c = string; *c != '\0'; c++) glutStrokeCharacter(font, *c);
-}
-
-
-// Drawing routine.
-void drawScene(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-
-    //gluLookAt goes here
-    gluLookAt(meX, meY, meZ,
-        meX + sin(angle * PI/180), meY, meZ + cos(angle * PI / 180),
-        0, 1, 0);
-
-
-
-    //Print the numbers in a circle.
-    //radius 50
-    //0 at (0,0,50)
-    //90 at (50,0,0)
-    glColor4f(0.0, 0.0, 0.0, 1.0);
-    for (int theta = 0; theta < 360; theta += 10)
-    {
-        glPushMatrix();
-        glTranslatef(50 * sin(theta * PI / 180), 0.0, 50 * cos(theta * PI / 180));
-        glRotatef(theta, 0, 1, 0);
-
-        glRotatef(180, 0.0, 1.0, 0.0); //line it up with 0 position
-        glScalef(0.025, 0.025, 0.025);
-
-        // convert a number to a string for printing
-        char numString[5];
-        sprintf(numString, "%d", theta);
-        writeStrokeString(GLUT_STROKE_ROMAN, numString);
-
-        glPopMatrix();
-    }
-
-
-    glutSwapBuffers();
-}
-
-void specialKeyInput(int key, int x, int y)
-{
-    switch (key) {
-    case GLUT_KEY_UP: //forward
-        meX += stepsize * sin(angle * PI / 180);
-        meZ += stepsize * cos(angle * PI / 180);
-        glutPostRedisplay();
-        break;
-    case GLUT_KEY_DOWN: //back
-        meX -= stepsize * sin(angle * PI / 180);
-        meZ -= stepsize * cos(angle * PI / 180);
-        glutPostRedisplay();
-        break;
-    case GLUT_KEY_RIGHT: //turn right
-        angle -= turnsize;
-        glutPostRedisplay();
-        break;
-    case GLUT_KEY_LEFT: //turn left
-        angle += turnsize;
-        glutPostRedisplay();
-        break;
-    }//end switch
-    glutPostRedisplay();
-}
-
-// Routine to output interaction instructions to the C++ window.
-void printInteraction(void)
-{
-    cout << "Interaction:" << endl;
-    cout << "Not yet implemented:" << endl;
-    cout << "\tup arrow - step forward" << endl;
-    cout << "\tdown arrow - step backward" << endl;
-    cout << "\tright arrow - turn to the right" << endl;
-    cout << "\tleft arrow - turn to the left" << endl;
-    cout << "\tr - reset to standing in the center ";
-    cout << "facing 0 degrees." << endl;
-}
-
-// Keyboard input processing routine.
-void keyInput(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-    case 27:
-        exit(0);
-        break;
-    case 'r':
-
-        break;
-    default:
-        break;
-    }
-}
-
-
-// Main routine.
-int main(int argc, char** argv)
-{
-    printInteraction();
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Walking around looking at numbers.");
-    setup();
-    glutDisplayFunc(drawScene);
-    glutReshapeFunc(resize);
-    glutKeyboardFunc(keyInput);
-    glutSpecialFunc(specialKeyInput);
-    glutMainLoop();
-
-    return 0;
-}
+////  This version has no lookat.
+//
+////  floor is xz plane, positive y is up.
+//
+////This define is so you can use sprintf
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include <iostream>
+//#include <stdio.h>
+//#include <cmath>
+//
+//#ifdef __APPLE__
+//#  include <GLUT/glut.h>
+//#else
+//#  include <GL/glut.h>
+//#endif
+//
+//#define PI 3.14159
+//using namespace std;
+//
+////I don't use these by you can
+//static float meX = 0, meY = 0, meZ = 0;
+//static float angle = 0;  //angle facing
+//static float stepsize = 5.0;  //step size
+//static float turnsize = 10.0; //degrees to turn
+//
+//// Initialization routine.
+//void setup(void)
+//{
+//    glClearColor(1.0, 1.0, 1.0, 0.0);
+//    glEnable(GL_DEPTH_TEST); // Enable depth testing.
+//}
+//
+//// OpenGL window reshape routine.
+//void resize(int w, int h)
+//{
+//    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    gluPerspective(120, 1, 1, 100);
+//    glMatrixMode(GL_MODELVIEW);
+//}
+//
+//// Routine to draw a stroke character string.
+//void writeStrokeString(void* font, const char* string)
+//{
+//    const char* c;
+//    for (c = string; *c != '\0'; c++) glutStrokeCharacter(font, *c);
+//}
+//
+//
+//// Drawing routine.
+//void drawScene(void)
+//{
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//    glLoadIdentity();
+//
+//    //gluLookAt goes here
+//    gluLookAt(meX, meY, meZ,
+//        meX + sin(angle * PI/180), meY, meZ + cos(angle * PI / 180),
+//        0, 1, 0);
+//
+//
+//
+//    //Print the numbers in a circle.
+//    //radius 50
+//    //0 at (0,0,50)
+//    //90 at (50,0,0)
+//    glColor4f(0.0, 0.0, 0.0, 1.0);
+//    for (int theta = 0; theta < 360; theta += 10)
+//    {
+//        glPushMatrix();
+//        glTranslatef(50 * sin(theta * PI / 180), 0.0, 50 * cos(theta * PI / 180));
+//        glRotatef(theta, 0, 1, 0);
+//
+//        glRotatef(180, 0.0, 1.0, 0.0); //line it up with 0 position
+//        glScalef(0.025, 0.025, 0.025);
+//
+//        // convert a number to a string for printing
+//        char numString[5];
+//        sprintf(numString, "%d", theta);
+//        writeStrokeString(GLUT_STROKE_ROMAN, numString);
+//
+//        glPopMatrix();
+//    }
+//
+//
+//    glutSwapBuffers();
+//}
+//
+//void specialKeyInput(int key, int x, int y)
+//{
+//    switch (key) {
+//    case GLUT_KEY_UP: //forward
+//        meX += stepsize * sin(angle * PI / 180);
+//        meZ += stepsize * cos(angle * PI / 180);
+//        glutPostRedisplay();
+//        break;
+//    case GLUT_KEY_DOWN: //back
+//        meX -= stepsize * sin(angle * PI / 180);
+//        meZ -= stepsize * cos(angle * PI / 180);
+//        glutPostRedisplay();
+//        break;
+//    case GLUT_KEY_RIGHT: //turn right
+//        angle -= turnsize;
+//        glutPostRedisplay();
+//        break;
+//    case GLUT_KEY_LEFT: //turn left
+//        angle += turnsize;
+//        glutPostRedisplay();
+//        break;
+//    }//end switch
+//    glutPostRedisplay();
+//}
+//
+//// Routine to output interaction instructions to the C++ window.
+//void printInteraction(void)
+//{
+//    cout << "Interaction:" << endl;
+//    cout << "Not yet implemented:" << endl;
+//    cout << "\tup arrow - step forward" << endl;
+//    cout << "\tdown arrow - step backward" << endl;
+//    cout << "\tright arrow - turn to the right" << endl;
+//    cout << "\tleft arrow - turn to the left" << endl;
+//    cout << "\tr - reset to standing in the center ";
+//    cout << "facing 0 degrees." << endl;
+//}
+//
+//// Keyboard input processing routine.
+//void keyInput(unsigned char key, int x, int y)
+//{
+//    switch (key)
+//    {
+//    case 27:
+//        exit(0);
+//        break;
+//    case 'r':
+//
+//        break;
+//    default:
+//        break;
+//    }
+//}
+//
+//
+//// Main routine.
+//int main(int argc, char** argv)
+//{
+//    printInteraction();
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+//    glutInitWindowSize(500, 500);
+//    glutInitWindowPosition(100, 100);
+//    glutCreateWindow("Walking around looking at numbers.");
+//    setup();
+//    glutDisplayFunc(drawScene);
+//    glutReshapeFunc(resize);
+//    glutKeyboardFunc(keyInput);
+//    glutSpecialFunc(specialKeyInput);
+//    glutMainLoop();
+//
+//    return 0;
+//}
